6c6ae980eb050902cfa9e204800985b97f592b20
Eleazar <poncebe@gmail.com>
2016-10-03 22:50:25 -0500

    Imagenes de pantallas añadidas, Carpeta Dimensiones con la ubicacion en px de cada objeto de la pantalla

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 ALLEGRO_DISPLAY *display;
 ALLEGRO_EVENT_QUEUE *event_queue;
-ALLEGRO_BITMAP *img_main, *img_mainStart, *img_mainExit, *img_getIP, *img_getIPCon;
+ALLEGRO_BITMAP *img_main, *img_mainStart, *img_mainExit, *img_getIP, *img_getIPCon, *img_downCards;
 ALLEGRO_FONT *font;
 ALLEGRO_USTR *strIPServer;
 
@@ -30,6 +30,7 @@ void destroyAll(){
     al_destroy_bitmap(img_mainExit);
     al_destroy_bitmap(img_getIP);
     al_destroy_bitmap(img_getIPCon);
+    al_destroy_bitmap(img_downCards);
     //Fuente
     al_destroy_font(font);
     //Strings
@@ -52,7 +53,8 @@ int main(){
     img_mainExit = al_load_bitmap("imgs/main_exit.png");
     img_getIP = al_load_bitmap("imgs/getIP.png");
     img_getIPCon = al_load_bitmap("imgs/getIPCon.png");
-    font = al_create_builtin_font();
+    img_downCards = al_load_bitmap("imgs/DownCards.png");
+
     strIPServer = al_ustr_new("");
     pos = (int)al_ustr_size(strIPServer);
 
@@ -117,11 +119,13 @@ int main(){
 
                         if(event2.mouse.x > 751 && event2.mouse.x < 1029 && event2.mouse.y > 543 && event2.mouse.y < 620){//Click Conectar
 
-                            bool terminar = false;
+                            //bool terminar = false;
 
-                            while(!terminar){
+                            al_draw_bitmap(img_downCards, 0, 0, 0);
+                            al_flip_display();
+                            //while(!terminar){
 
-                            }
+                            //}
                         }
                         if(event2.mouse.x > 341 && event2.mouse.x < 768 && event2.mouse.y > 344 &&event2.mouse.y < 424){//Click cuadro IP
                                 al_draw_ustr(font, al_map_rgb_f(0, 0, 0), 330, 170, 0, strIPServer);
