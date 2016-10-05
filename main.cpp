219b3e8aa58d78a57768b6137c8bc9bf5267c446
Eleazar Ponce <poncebe@gmail.com>
2016-10-05 18:53:08 -0500

    Interfaz terminada. Solo falta capturara texto.

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ using namespace std;
 
 ALLEGRO_DISPLAY *display;
 ALLEGRO_EVENT_QUEUE *event_queue;
-ALLEGRO_BITMAP *img_main, *img_mainStart, *img_mainExit, *img_getIP, *img_getIPCon, *img_downCards, *img_getIPBox, *img_getIPBoxCon;
-ALLEGRO_BITMAP *img_UbuntuCard, *img_ArchCard, *img_DebianCard, *img_FedoraCard, *img_SuseCard;
+ALLEGRO_BITMAP *img_main, *img_mainStart, *img_mainExit, *img_getIP, *img_getIPCon, *img_Tablero, *img_getIPBox, *img_getIPBoxCon;
+ALLEGRO_BITMAP *img_UbuntuCard, *img_ArchCard, *img_DebianCard, *img_FedoraCard, *img_SuseCard, *img_DownCard;
 ALLEGRO_FONT *font;
 ALLEGRO_USTR *strIPServer;
 
@@ -32,7 +32,7 @@ void destroyAll(){
     al_destroy_bitmap(img_mainExit);
     al_destroy_bitmap(img_getIP);
     al_destroy_bitmap(img_getIPCon);
-    al_destroy_bitmap(img_downCards);
+    al_destroy_bitmap(img_Tablero);
     al_destroy_bitmap(img_getIPBox);
     al_destroy_bitmap(img_getIPBoxCon);
     al_destroy_bitmap(img_UbuntuCard);
@@ -40,6 +40,7 @@ void destroyAll(){
     al_destroy_bitmap(img_FedoraCard);
     al_destroy_bitmap(img_ArchCard);
     al_destroy_bitmap(img_SuseCard);
+    al_destroy_bitmap(img_DownCard);
     //Fuente
     al_destroy_font(font);
     //Strings
@@ -62,7 +63,7 @@ int main(){
     img_mainExit = al_load_bitmap("imgs/main_exit.png");
     img_getIP = al_load_bitmap("imgs/getIP.png");
     img_getIPCon = al_load_bitmap("imgs/getIPCon.png");
-    img_downCards = al_load_bitmap("imgs/DownCards.png");
+    img_Tablero = al_load_bitmap("imgs/Tablero.png");
     img_getIPBox = al_load_bitmap("imgs/getIPBox.png");
     img_getIPBoxCon = al_load_bitmap("imgs/getIPBoxCon.png");
     img_UbuntuCard = al_load_bitmap("imgs/UbuntuCard.png");
@@ -70,6 +71,7 @@ int main(){
     img_FedoraCard = al_load_bitmap("imgs/FedoraCard.png");
     img_ArchCard = al_load_bitmap("imgs/ArchCard.png");
     img_SuseCard = al_load_bitmap("imgs/SuseCard.png");
+    img_DownCard = al_load_bitmap("imgs/DownCard.png");
 
     strIPServer = al_ustr_new("");
     pos = (int)al_ustr_size(strIPServer);
@@ -138,13 +140,90 @@ int main(){
                         if(event2.mouse.x > 812 && event2.mouse.x < 1111 && event2.mouse.y > 557 && event2.mouse.y < 638){///Click Conectar
 
                             bool gameOver = false;
+                            bool card1 = false, card2 = false, card3 = false, card4 = false, card5 = false;
+                            bool card6 = false, card7 = false, card8 = false, card9 = false, card10 = false;
 
-                            al_draw_bitmap(img_downCards, 0, 0, 0);
+                            al_draw_bitmap(img_Tablero, 0, 0, 0);
                             al_flip_display();
                             while(!gameOver){
 
                                 ALLEGRO_EVENT event;
                                 al_wait_for_event(event_queue, &event);
+
+                                if(card1){
+                                    al_draw_bitmap(img_UbuntuCard, 163, 132, 0);
+                                    al_flip_display();
+                                }else{
+                                    al_draw_bitmap(img_DownCard, 163, 132, 0);
+                                    al_flip_display();
+                                }
+
+                                if(card2){
+                                    al_draw_bitmap(img_DebianCard, 342, 132, 0);
+                                    al_flip_display();
+                                }else{
+                                    al_draw_bitmap(img_DownCard, 342, 132, 0);
+                                    al_flip_display();
+                                }
+
+                                if(card3){
+                                    al_draw_bitmap(img_ArchCard, 515, 132, 0);
+                                    al_flip_display();
+                                }else{
+                                    al_draw_bitmap(img_DownCard, 515, 132, 0);
+                                    al_flip_display();
+                                }
+
+                                if(card4){
+                                    al_draw_bitmap(img_SuseCard, 699, 132, 0);
+                                    al_flip_display();
+                                }else{
+                                    al_draw_bitmap(img_DownCard, 699, 132, 0);
+                                    al_flip_display();
+                                }
+
+                                if(card5){
+                                    al_draw_bitmap(img_FedoraCard, 880, 132, 0);
+                                    al_flip_display();
+                                }else{
+                                    al_draw_bitmap(img_DownCard, 880, 132, 0);
+                                    al_flip_display();
+                                }
+                                ///*********************///
+                                if(card6){
+
+                                }else{
+                                    al_draw_bitmap(img_DownCard, 162, 356, 0);
+                                    al_flip_display();
+                                }
+
+                                if(card7){
+
+                                }else{
+                                    al_draw_bitmap(img_DownCard, 342, 356, 0);
+                                    al_flip_display();
+                                }
+
+                                if(card8){
+
+                                }else{
+                                    al_draw_bitmap(img_DownCard, 515, 356, 0);
+                                    al_flip_display();
+                                }
+
+                                if(card9){
+
+                                }else{
+                                    al_draw_bitmap(img_DownCard, 880, 356, 0);
+                                    al_flip_display();
+                                }
+
+                                if(card10){
+
+                                }else{
+                                    al_draw_bitmap(img_DownCard, 699, 356, 0);
+                                    al_flip_display();
+                                }
 
                                 if(event.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
                                     gameOver = true;
@@ -159,17 +238,24 @@ int main(){
                                 if(event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN){///Clicks en el tablero
 
                                     if(event.mouse.x > 163 && event.mouse.x < 312 && event.mouse.y > 132 && event.mouse.y < 340){///Carta 1
-                                        al_draw_bitmap(img_UbuntuCard, 163, 132, 0);
-                                        al_flip_display();
+                                        card1 = true;
                                     }
                                     if(event.mouse.x > 342 && event.mouse.x < 491 && event.mouse.y > 132 && event.mouse.y < 340){///Carta 2
-                                        al_destroy_bitmap(img_UbuntuCard);
-                                        al_flip_display();
+                                        card2 = true;
+                                    }
+                                    if(event.mouse.x > 515 && event.mouse.x < 664 && event.mouse.y > 132 && event.mouse.y < 340){///Carta 3
+                                        card3 = true;
+                                    }
+                                    if(event.mouse.x > 699 && event.mouse.x < 848 && event.mouse.y > 132 && event.mouse.y < 340){///Carta 4
+                                        card4 = true;
+                                    }
+                                    if(event.mouse.x > 880 && event.mouse.x < 1029 && event.mouse.y > 132 && event.mouse.y < 340){///Carta 4
+                                        card5 = true;
                                     }
                                 }
                             }
                         }
-                        if(event2.mouse.x > 341 && event2.mouse.x < 768 && event2.mouse.y > 344 &&event2.mouse.y < 424){//Click cuadro IP
+                        if(event2.mouse.x > 341 && event2.mouse.x < 768 && event2.mouse.y > 344 &&event2.mouse.y < 424){///Click cuadro IP
                                 clickOnBox = true;
                                 al_draw_bitmap(img_getIPBox, 0, 0, 0);
                                 al_flip_display();
